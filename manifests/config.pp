8ac4f8f1d050a4895e8e3d00f29c497b1c10966a
root <root@prod-puppetserver01.healthplan.com>
2016-11-11 13:12:30 -0500

    Added support for s390x architecture which has a different cron.daily script.

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,20 +4,44 @@ class logrotate::config{
   $manage_cron_daily = $::logrotate::manage_cron_daily
   $config            = $::logrotate::config
 
+  $file_owner        = 'root'
+  $file_group        = 'root'
+
+  case $::osfamily {
+    'RedHat' : {
+      $file_mode = $::operatingsystemmajrelease ? {
+        5       => '0755',
+        default => '0700',
+      }
+      $dir_mode = '0644'
+    }
+    default: {
+      $file_mode = '0555'
+      $dir_mode  = '0755'
+    }
+  }
+
+  # RedHat 7 running on IBM S390x architecture, the cron.daily script is different.
+  if ($::osfamily == 'RedHat') and ($::operatingsystemmajrelease == '7') and ($::architecture == 's390x') {
+    $cron_daily_script = 'logrotate.s390x'
+  } else {
+    $cron_daily_script = 'logrotate'
+  }
+
   file{'/etc/logrotate.d':
     ensure => directory,
     owner  => 'root',
     group  => 'root',
-    mode   => '0755',
+    mode   => $dir_mode,
   }
 
   if $manage_cron_daily {
     file{'/etc/cron.daily/logrotate':
       ensure => file,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0555',
-      source => 'puppet:///modules/logrotate/etc/cron.daily/logrotate',
+      owner  => $file_owner,
+      group  => $file_group,
+      mode   => $file_mode,
+      source => "puppet:///modules/logrotate/etc/cron.daily/${cron_daily_script}",
     }
   }
 
