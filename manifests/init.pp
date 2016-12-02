2165c970f18ece52c8ad5aa272f74aab0c64d302
Travis Fields <travis.fields@nike.com>
2016-12-02 14:07:11 -0800

    Use datatypes and upgrade to > 4.0
    Simply logic with the prepending 'no' accordingly and iterating in erb instead of puppet
    
    Update to puppet 4 or greater, add data types instead of all the validation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,26 @@
 #
 class logrotate (
-  $ensure            = present,
-  $hieramerge        = false,
-  $manage_cron_daily = true,
-  $package           = 'logrotate',
-  $rules             = {},
-  $config            = undef,
-) {
+  Enum[
+    'present',
+    'absent',
+    'latest']      $ensure            = present,
+  Boolean          $hieramerge        = false,
+  Boolean          $manage_cron_daily = true,
+  String           $package           = 'logrotate',
+  Hash             $rules             = { },
+  Optional[String] $config            = undef,
+) inherits logrotate::params {
 
   include ::logrotate::install
   include ::logrotate::config
   include ::logrotate::rules
   include ::logrotate::defaults
 
-  anchor{'logrotate_begin':}->
+  anchor { 'logrotate_begin': } ->
   Class['::logrotate::install']->
   Class['::logrotate::config']->
   Class['::logrotate::rules']->
   Class['::logrotate::defaults']->
-  anchor{'logrotate_end':}
+  anchor { 'logrotate_end': }
 
 }
