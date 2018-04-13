2dfc95d5181302f0d27cf88cde1ecd48690d6e98
Colin Wood <cwood06@gmail.com>
2018-04-13 10:34:23 -0700

    Hieramerge was never used adding a fix

--- manifests/rules.pp
+++ manifests/rules.pp
@@ -1,7 +1,13 @@
 # apply user-defined rules
-class logrotate::rules ($rules = $::logrotate::rules){
+class logrotate::rules {
 
   assert_private()
+
+  if $::logrotate::hieramerge {
+      $rules = lookup("logrotate::rules", Hash, 'deep', {})
+  } else {
+      $rules = $::logrotate::rules
+  }
 
   create_resources('logrotate::rule', $rules)
 
