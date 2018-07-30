2c3a23e888e8e404e62407a7b2e514946bc1e820
Arjen Heidinga <dexter@beetjevreemd.nl>
2018-07-30 12:21:52 +0200

    fix: Unintended change.

--- spec/defines/conf_spec.rb
+++ spec/defines/conf_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'logrotate::conf' do
   _, facts = on_supported_os.first
+  let(:facts) { facts } 
 
   shared_examples 'error raised' do |param, _|
     context "=> 'foo'" do
