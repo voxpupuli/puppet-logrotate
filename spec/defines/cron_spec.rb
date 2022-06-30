755ea3fd520fbf6df20c87445a44f68ab195a255
Tim Meusel <tim@bastelfreak.de>
2022-06-30 15:11:00 +0200

    cron_spec.rb: Cleanup tests

--- spec/defines/cron_spec.rb
+++ spec/defines/cron_spec.rb
@@ -52,46 +52,5 @@ describe 'logrotate::cron' do
         end
       end
     end
-
-    # Test FreeBSD separately as it is only partially supported by the module and not in the list of supported os.
-    # When FreeBSD is added to the list of supported systems, these tests can be removed as they are already part of the test set above.
-    context 'on FreeBDS' do
-      let(:facts) { { os: { family: 'FreeBSD' } } }
-
-      context 'With default params' do
-        let(:title) { 'test' }
-        let(:params) { { ensure: 'present' } }
-
-        it {
-          is_expected.to contain_file('/usr/local/bin/logrotate.test.sh').
-            with_ensure('present').
-            with_content(%r{(/usr/local/sbin/logrotate /usr/local/etc/logrotate.conf 2>&1)})
-        }
-      end
-
-      context 'With additional arguments and status' do
-        let(:pre_condition) { 'class {"::logrotate": logrotate_args => ["-s /var/lib/logrotate/logrotate.status", "-m /usr/sbin/mailer"]}' }
-        let(:title) { 'test' }
-        let(:params) { { ensure: 'present' } }
-
-        it {
-          is_expected.to contain_file('/usr/local/bin/logrotate.test.sh').
-            with_ensure('present').
-            with_content(%r{(/usr/local/sbin/logrotate -s /var/lib/logrotate/logrotate.status -m /usr/sbin/mailer /usr/local/etc/logrotate.conf 2>&1)})
-        }
-      end
-
-      context 'With additional arguments' do
-        let(:pre_condition) { 'class {"::logrotate": cron_always_output => true}' }
-        let(:title) { 'test' }
-        let(:params) { { ensure: 'present' } }
-
-        it {
-          is_expected.to contain_file('/usr/local/bin/logrotate.test.sh').
-            with_ensure('present').
-            with_content(%r{(else\n    echo "\${OUTPUT}"\nfi)})
-        }
-      end
-    end
   end
 end
